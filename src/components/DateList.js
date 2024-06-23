696f1ee99a65f60ae92938d2de19d9a655ec3ec8
VladimirPoplavsky <vovik.ds@gmail.com>
2024-06-23 11:58:08 +0300

    1 - remove from output name of day 2 - change output format should be 2 digits of day, 2 digits of month and 4 digits of year for example 1 january 192 year should look 01/01/0192 3 - reformat output instead MM/DD/YYYY will be DD/MM/YYYY

--- src/components/DateList.js
+++ src/components/DateList.js
@@ -1,10 +1,10 @@
+// src/components/DateList.js
 import React from 'react';
 import CopyButton from './CopyButton';
 
 const DateList = ({ dates }) => {
-    // Function to format dates for copying to Excel
     const formatDatesForCopy = () => {
-        return dates.map(item => `${item.date.toLocaleDateString()} - ${item.dayName}`).join('\r\n');
+        return dates.map(item => `${item.date}`).join('\n');
     };
 
     return (
@@ -13,7 +13,7 @@ const DateList = ({ dates }) => {
             <ul>
                 {dates.map((item, index) => (
                     <li key={index}>
-                        {item.date.toLocaleDateString()}, {item.dayName}
+                        {item.date}
                     </li>
                 ))}
             </ul>
