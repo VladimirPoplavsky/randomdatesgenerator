00c10fdb2b1fb6cc542f9b93220811361b6430a1
VladimirPoplavsky <vovik.ds@gmail.com>
2024-06-23 21:08:55 +0300

    add end date to generate dates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-// src/App.js
 import React, { useState } from 'react';
 import DateInput from './components/DateInput';
 import DateList from './components/DateList';
 import { generateRandomDates } from './utils/dateUtils';
 import './App.css';
 
-
 const App = () => {
     const [dates, setDates] = useState([]);
 
-    const handleGenerateDates = (startDate, rowCount) => {
-        const randomDates = generateRandomDates(startDate, rowCount);
-        setDates(randomDates);
+    const handleGenerateDates = (startDate, endDate, rowCount) => {
+        const generatedDates = generateRandomDates(new Date(startDate), new Date(endDate), rowCount);
+        setDates(generatedDates);
     };
 
     return (
-        <div className={"App"}>
+        <div className="App">
             <h1>Random Date Generator</h1>
-            <DateInput onGenerate={handleGenerateDates} />
+            <DateInput onGenerateDates={handleGenerateDates} />
             <DateList dates={dates} />
         </div>
     );
